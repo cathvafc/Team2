3bf27cfd51e4436806262b0e6efb19243da85e68
cathvafc <miguelangelgomezlope@gmail.com>
2021-03-03 20:47:55 +0100

    Added Niv_Org

--- Team2Solution/Team2Solution/Controllers/TrabajadoresController.cs
+++ Team2Solution/Team2Solution/Controllers/TrabajadoresController.cs
@@ -6,14 +6,46 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Team2.Models;
+using Team2.DTO;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq.Expressions;
 
-namespace Team2Solution.Controllers
+namespace Team2.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TrabajadoresController : ControllerBase
     {
         private readonly APIContext _context;
+
+        //DTOs
+
+        private static readonly Expression<Func<Trabajadores, TrabajadoresDto>> AsTablaTrabajadoresDto =
+            
+            t => new TrabajadoresDto
+
+            {
+                CAMINO = t.NivelOrganizativo.Camino,
+                ID_TRABAJADOR = t.ID_TRABAJADOR.ToString(),
+                EMPRESA = t.empresa.D_EMPRESA,
+                NOMBRE = t.NOMBRE + " " + t.APELLIDO1 + " " + t.APELLIDO2,
+                TP = t.t_Provincia.ID_CLASE_PER.ToString(),
+                TIPO_EMPRESA = t.t_Provincia.DESCRIP,
+                GRUPO = t.GRUPO.ToString(),
+                CUERPO = t.Cuerpo.DESCRIP,
+                CATEGORIA = t.categorias.DESCRIP
+
+            };
+
+        private static readonly Expression<Func<NivOrg, DepartamentoDto>> AsTablaDepartamentoDto =
+
+            d => new DepartamentoDto
+
+            {          
+                CAMINO = d.Camino,
+                NOMBRE = d.DNivel
+            };
 
         public TrabajadoresController(APIContext context)
         {
